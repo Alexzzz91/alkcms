f8cd1dd4d159f5c71a32e6178889713dc728e69f
alk <belalex.9132788@gmail.com>
2017-04-19 02:26:47 +0600

    added more frontpages

--- src/routes/frontpage.js
+++ src/routes/frontpage.js
@@ -1,3 +1,10 @@
 exports.get = function(req, res) {
-  res.render('frontpage');
+	if(req.user == null){
+		//var number = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
+		var number = 4;
+		res.render('frontpage'+number);
+	}else{
+		res.render('dashboard');
+	}
+  	
 };
